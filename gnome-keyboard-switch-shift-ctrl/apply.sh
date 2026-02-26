674df7f2a1d6541355cc87a8456511fac892d4fb
kosmos <sunfundev@gmail.com>
2026-02-26 18:23:24 +0300

    add gnome-keyboard-switch-shift-ctrl

---
+++ gnome-keyboard-switch-shift-ctrl/apply.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Control>Shift_L']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift>Control_L']"
